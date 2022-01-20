61e052efcd65d0ec61a6129e291588cd6bdde676
Mitier26 <mitier26@gmail.com>
2022-01-20 15:18:23 +0900

    배달 게임

---
+++ Delivery Driver/Assets/Driver.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driver : MonoBehaviour
+{
+    [SerializeField] float steerSpeed = 150f;
+    [SerializeField] float moveSpeed = 7f;
+    [SerializeField] float slowSpeed = 5f;
+    [SerializeField] float boostSpeed = 10f;
+
+
+    void Start()
+    {
+        //한
+    }
+
+
+    void Update()
+    {
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+
+        transform.Rotate(0, 0, -steerAmount );
+        transform.Translate(0, moveAmount, 0);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        moveSpeed = slowSpeed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Boost")
+        {
+            moveSpeed = boostSpeed;
+        }
+    }
+
+}
