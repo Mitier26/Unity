f1408ceb97d916a5158a86f0187b39f3cfd17830
Mitier26 <mitier26@gmail.com>
2022-01-21 14:43:25 +0900

    회전

---
+++ Snow Boarder/Assets/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb2d;
+
+    [SerializeField] float torqueAmount = 1f;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+       if(Input.GetKey(KeyCode.A))
+        {
+            rb2d.AddTorque(torqueAmount);
+        }
+       else if (Input.GetKey(KeyCode.D))
+        {
+            rb2d.AddTorque(-torqueAmount);
+        }
+    }
+}
