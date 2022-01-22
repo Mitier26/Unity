84f4d5e0d76ca91ffe678e10e1171a640732644d
Mitier26 <mitier26@gmail.com>
2022-01-22 21:50:45 +0900

    파티클 시스템

--- Snow Boarder/Assets/CrashlDetector.cs
+++ Snow Boarder/Assets/CrashlDetector.cs
@@ -6,11 +6,13 @@ using UnityEngine.SceneManagement;
 public class CrashlDetector : MonoBehaviour
 {
     [SerializeField] float loadDelay = 0.5f;
+    [SerializeField] ParticleSystem crashEffect;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Ground")
         {
+            crashEffect.Play();
             Invoke("ReloadScene", loadDelay);            
         }
     }
