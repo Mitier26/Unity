339f00aafbb7e1dfd7d5c9146c63451c267e843e
Mitier26 <mitier26@gmail.com>
2022-01-23 15:36:54 +0900

    타이머

--- QuizQuiz/Assets/Quiz.cs
+++ QuizQuiz/Assets/Quiz.cs
@@ -6,20 +6,70 @@ using TMPro;
 
 public class Quiz : MonoBehaviour
 {
+    [Header("Questions")]
     [SerializeField] TextMeshProUGUI questionText;  //질문을 출력하는 것
     [SerializeField] QuestionSO question;           //퀴즈의 정보를 가지고 있는 ScriptabelObject
+
+    [Header("Answers")]
     [SerializeField] GameObject[] answerButtons;     //선택지 4개
     int correctAnswerIndex;                         //정답의 인덱스
+    bool hasAnswereEarly;
+
+    [Header("Button Colors")]
     [SerializeField] Sprite defaultAnswerSprite;    //기본 그림
     [SerializeField] Sprite correctAnswerSprite;    //정답 그림
 
+    [Header("Timer")]
+    [SerializeField] Image timerImage;
+    Timer timer;
+
     private void Start()
     {
+        timer = FindObjectOfType<Timer>();
         DisplayQuestion();
 
     }
 
+    private void Update()
+    {
+        timerImage.fillAmount = timer.fillFraction;
+        if(timer.loadNextQuestion)
+        {
+            hasAnswereEarly = false;
+            GetNextQuestion();
+            timer.loadNextQuestion = false;
+        }
+        else if(!hasAnswereEarly && !timer.isAnsweringQuestion)
+        {
+            DisplayAnswer(-1);
+            SetButtonState(false);
+        }
+    }
+
     public void OnAnswerSelected(int index) // 버튼 클릭에 사용할 것
+    {
+        hasAnswereEarly = true;
+        DisplayAnswer(index);
+
+        SetButtonState(false);
+        timer.CancleTimer();
+    }
+
+    void DisplayQuestion()
+    {
+        questionText.text = question.GetQuestion();
+        // ScriptableObject에서 문제의 정보를 가지고 와 출력
+
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
+            buttonText.text = question.GetAnswer(i);
+            //각 버튼의 TextMeshPro 컴포넌트를 저장
+            //각 버튼에 ScriptableObject의 선택지를 출력
+        }
+    }
+
+    void DisplayAnswer(int index)
     {
         Image buttonImage;
 
@@ -37,25 +87,9 @@ public class Quiz : MonoBehaviour
             buttonImage = answerButtons[correctAnswerIndex].GetComponent<Image>();
             buttonImage.sprite = correctAnswerSprite;
         }
-
-        SetButtonState(false);
     }
 
-    void DisplayQuestion()
-    {
-        questionText.text = question.GetQuestion();
-        // ScriptableObject에서 문제의 정보를 가지고 와 출력
-
-        for (int i = 0; i < answerButtons.Length; i++)
-        {
-            TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
-            buttonText.text = question.GetAnswer(i);
-            //각 버튼의 TextMeshPro 컴포넌트를 저장
-            //각 버튼에 ScriptableObject의 선택지를 출력
-        }
-    }
-
-    void GetNextQuestion()
+    void GetNextQuestion()  // 버튼의 상태를 기본값으로 변경
     {
         SetButtonState(true);
         SetDefaultButtonSprites();
