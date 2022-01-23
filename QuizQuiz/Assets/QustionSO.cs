81014a5dd00a9600dd268d53325bf0832102736f
Mitier26 <mitier26@gmail.com>
2022-01-23 09:41:10 +0900

    스트립트 오브젝트

---
+++ QuizQuiz/Assets/QustionSO.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "QuizQuestion", fileName = "New Question")]
+public class QustionSO : ScriptableObject
+{
+    [TextArea(2,6)]
+    [SerializeField] string question = "여기에 문제를 입력 하세요";
+    [SerializeField] string[] answers = new string[4];
+    [Range(1,4)]
+    [SerializeField] int correctAnswerIndex;
+
+    public string GetQuestion()
+    {
+        return question;
+    }
+
+    public string GetAnswer(int index)
+    {
+        return answers[index];
+    }
+
+    public int GetCorrectAnswerIndex()
+    {
+        return correctAnswerIndex;
+    }
+
+}
