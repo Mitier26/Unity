b2281c763c6b126129ac3b91f156c060390a87e3
Mitier26 <mitier26@gmail.com>
2022-01-23 10:38:50 +0900

    텍스트 적용

--- QuizQuiz/Assets/QuestionSO.cs
+++ QuizQuiz/Assets/QuestionSO.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// 팝업 메뉴에 보여지는 것
+// 팝업 메뉴에 QuizQuestion 이라고 보이고
+// 파일을 만들면 New Question 이라고 생성된다.
 [CreateAssetMenu(menuName = "QuizQuestion", fileName = "New Question")]
-public class QustionSO : ScriptableObject
+public class QuestionSO : ScriptableObject   // MonoBehaviour 가 아니다
 {
-    [TextArea(2,6)]
+    [TextArea(2,6)] // 글을 입력하는 곳이 1줄이 아니고 6줄이 된다.
     [SerializeField] string question = "여기에 문제를 입력 하세요";
     [SerializeField] string[] answers = new string[4];
-    [Range(1,4)]
     [SerializeField] int correctAnswerIndex;
 
     public string GetQuestion()
---
+++ QuizQuiz/Assets/Quiz.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Quiz : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI questionText;
+    [SerializeField] QuestionSO question;
+    [SerializeField] GameObject[] answerPrefab;
+
+    private void Start()
+    {
+        questionText.text = question.GetQuestion();
+
+        for(int i = 0; i <answerPrefab.Length; i++)
+        {
+            TextMeshProUGUI buttonText = answerPrefab[i].GetComponentInChildren<TextMeshProUGUI>();
+            buttonText.text = question.GetAnswer(i);
+        }
+
+        
+    }
+}
