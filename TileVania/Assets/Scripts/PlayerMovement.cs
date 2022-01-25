c4373060a93a1b2e033cfb86a58bd2bf9b89ce6c
Mitier26 <mitier26@gmail.com>
2022-01-25 23:09:02 +0900

    플렛포머

---
+++ TileVania/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float runSpeed = 10;
+    [SerializeField] float jumpSpeed = 5f;
+    Vector2 moveInput;
+    Rigidbody2D rigidbody;
+    Animator animator;
+    CapsuleCollider2D capsuleCollider;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        capsuleCollider = GetComponent<CapsuleCollider2D>();
+    }
+
+    private void Update()
+    {
+        Run();
+        FlipSprite();
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
+    // 점프에 관한 것
+    void OnJump(InputValue value)
+    {
+        if(!capsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            return;
+        }
+        if(value.isPressed)
+        {
+            rigidbody.velocity += new Vector2(0f, jumpSpeed);
+        }
+    }
+
+    void Run()
+    {
+        Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, rigidbody.velocity.y);
+        rigidbody.velocity = playerVelocity;
+
+        // 이동 애니메이션
+        bool playerHasHorizontalSpeed = Mathf.Abs(rigidbody.velocity.x) > Mathf.Epsilon;
+        animator.SetBool("IsRunning", playerHasHorizontalSpeed);
+    }
+
+    void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(rigidbody.velocity.x) > Mathf.Epsilon;
+
+        if(playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(rigidbody.velocity.x), 1f);
+        }
+
+    }
+}
