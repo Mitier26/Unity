b499f8bc6598c67a1b82262f1c36506890d72814
Mitier26 <mitier26@gmail.com>
2022-02-11 14:58:21 +0900

    과일 자르기 준비

---
+++ FruitNinja/Assets/Scripts/Fruit.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fruit : MonoBehaviour
+{
+    public GameObject slicedFruitPrefab;
+    public float explosionRadius = 5f;
+
+    public void CreateSlicedFruit()
+    {
+        GameObject inst = Instantiate(slicedFruitPrefab, transform.position, transform.rotation);
+
+        Rigidbody[] rbOnSliced = inst.transform.GetComponentsInChildren<Rigidbody>();
+
+        foreach(Rigidbody rigidbody in rbOnSliced)
+        {
+            rigidbody.transform.rotation = Random.rotation;
+            rigidbody.AddExplosionForce(Random.Range(500, 1000), transform.position, explosionRadius);
+        }
+
+        Destroy(gameObject);
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            CreateSlicedFruit();
+        }        
+    }
+}
