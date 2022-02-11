f44a52d37053d9a58c84fd172d01234fbc789aad
Mitier26 <mitier26@gmail.com>
2022-02-11 10:09:40 +0900

    final

--- RollingWeapon/Assets/Scripts/Cannon.cs
+++ RollingWeapon/Assets/Scripts/Cannon.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class Cannon : MonoBehaviour
 {
+    // 포탑 횐전을 위한 비어있는 오브젝트
     [SerializeField] Transform cannonHead;
+    // 레이저가 나가는 위치
     [SerializeField] Transform cannonTip;
+    // 공격 대기 시간
     [SerializeField] float shootingCoolDown = 3f;
     [SerializeField] float laserPower = 30;
 
@@ -25,18 +28,18 @@ public class Cannon : MonoBehaviour
 
     void Update()
     {
-        if(isPlayerInRange)
+        if(isPlayerInRange) // 범위에 플레이어가 들어 오면
         {
-            cannonHead.transform.LookAt(player.transform);
+            cannonHead.transform.LookAt(player.transform);  // 플레이어를 바라본다.
 
+            // 라인렌더라를 그린다.
             cannonLaser.SetPosition(0, cannonTip.transform.position);
-
             cannonLaser.SetPosition(1, player.transform.position);
-
+            // 0은 캐논의 시작점, 1는 플래이어의 위치
             timeLeftToShoot -= Time.deltaTime;
         }
 
-        if(timeLeftToShoot <= shootingCoolDown * 0.5f)
+        if(timeLeftToShoot <= shootingCoolDown * 0.5f)  // 3초의 반 1.5초
         {
             cannonLaser.sharedMaterial.color = Color.red;
         }
--- RollingWeapon/Assets/Scripts/Key.cs
+++ RollingWeapon/Assets/Scripts/Key.cs
@@ -4,13 +4,9 @@ using UnityEngine;
 
 public class Key : MonoBehaviour
 {
+    // 문에 열결해 문에 있는 것을 접근하기위해
     [SerializeField] Door doorToUnlock;
     [SerializeField] float keyRotationSpeed;
-
-    void Update()
-    {
-        //transform.Rotate(Vector3.up * Time.deltaTime * keyRotationSpeed);
-    }
 
     void OnTriggerEnter(Collider other)
     {
@@ -21,17 +17,22 @@ public class Key : MonoBehaviour
         }
     }
 
+    // 화면에 그림을 그리는 부분 Game 창에는 보이지 않는다.
     void OnDrawGizmos()
     {
         if(doorToUnlock != null)
         {
+            // 문과 연결되어 있으면 녹색선
             Gizmos.color = Color.green;
             Gizmos.DrawRay(transform.position, doorToUnlock.transform.position - transform.position);
         }
         else
         {
+            // 문과 연결되어 있지 않다면 빨간원
             Gizmos.color = Color.red;
             Gizmos.DrawSphere(transform.position + Vector3.up * 2, 0.5f);
         }
+        // 을 보여준다.
+        // 스트립트 에러를 위해 사용하면 좋다.
     }
 }
