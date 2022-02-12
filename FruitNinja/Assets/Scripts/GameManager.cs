abbf72eb25c6b81f0d190d42da446d8a27fd4e37
Mitier26 <mitier26@gmail.com>
2022-02-13 00:48:11 +0900

    게임오버UI

--- FruitNinja/Assets/Scripts/GameManager.cs
+++ FruitNinja/Assets/Scripts/GameManager.cs
@@ -2,12 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    [Header("점수")]
     int score;
     public Text scoreText;
     public Text highScoreText;
+
+    [Header("게임오버")]
+    public GameObject gameOverPanel;
 
     public void IncreaseScore(int addedPoints)
     {
@@ -19,5 +24,14 @@ public class GameManager : MonoBehaviour
     {
         // 게임 정지
         Time.timeScale = 0;
+        gameOverPanel.SetActive(true);
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        gameOverPanel.SetActive(false);
+
     }
 }
