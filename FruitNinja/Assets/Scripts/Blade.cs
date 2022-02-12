74681b5ddd1a2d0760046213a4f710839f3498c4
Mitier26 <mitier26@gmail.com>
2022-02-13 02:23:44 +0900

    마우스 움직임 확인

--- FruitNinja/Assets/Scripts/Blade.cs
+++ FruitNinja/Assets/Scripts/Blade.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Blade : MonoBehaviour
 {
+    public float minVelo = 0.1f;
     Rigidbody2D rb;
+    Vector3 lastMousePos;
+    Vector3 moveVelo;
+    Collider2D collider;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        collider = GetComponent<Collider2D>();
     }
 
-    void Update()
+    void FixedUpdate()
     {
+        collider.enabled = IsMouseMoving();
         SetBladeToMouse();
     }
 
@@ -25,5 +31,21 @@ public class Blade : MonoBehaviour
         // 마우스의 위치를 가지고 온다.
         // 마우스의 위치에서 10을 더한다. 더하는 값은 카메라의 위치이다.
         // Rigidbody의 위치를 변경한다.
+    }
+
+    bool IsMouseMoving()
+    {
+        Vector3 curMousePos = transform.position;
+        float traveled = (lastMousePos - curMousePos).magnitude;
+
+        lastMousePos = curMousePos;
+        if(traveled > minVelo)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
