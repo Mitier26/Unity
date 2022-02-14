2f0dcdfedf8cf221903365f2e5e4144d0d6a94c8
Mitier26 <mitier26@gmail.com>
2022-02-14 15:54:18 +0900

    이동

--- ZombieFPS/Assets/Scripts/MouseLook.cs
+++ ZombieFPS/Assets/Scripts/MouseLook.cs
@@ -10,6 +10,9 @@ public class MouseLook : MonoBehaviour
 
     void Start()
     {
+        // 화면과 마우스 이동을 고정하는 것
+        // 이것이 없으면 화면 따로 마우스 따로 움직이고.
+        // 창 모드의 경우 마우스가 창 밖으로 나간다.
         Cursor.lockState = CursorLockMode.Locked;
     }
 
@@ -18,7 +21,9 @@ public class MouseLook : MonoBehaviour
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitvity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitvity * Time.deltaTime;
         
+        // Y 값은 반전 되어 있으므로 - 를 해 주어야 한다.
         xRotation -= mouseY;
+        // 값이 90도 이상으로 움직이지 못하게 하는 것
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
