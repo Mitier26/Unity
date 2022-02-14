94e96aa02e5b76c5b37c69588bf214e47ca99338
Mitier26 <mitier26@gmail.com>
2022-02-15 00:39:19 +0900

    좀비 체력

--- ZombieFPS/Assets/Scripts/EnemyManager.cs
+++ ZombieFPS/Assets/Scripts/EnemyManager.cs
@@ -9,6 +9,17 @@ public class EnemyManager : MonoBehaviour
     public GameObject player;
     public Animator enemyAnimator;
     public float damage = 20f;
+    public float health = 100f;
+
+    public void Hit(float damage)
+    {
+        health -= damage;
+
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
 
     void Start()
     {
---
+++ ZombieFPS/Assets/Scripts/WeaponManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponManager : MonoBehaviour
+{
+    public GameObject playerCam;
+    public float range = 100f;
+    public float damage = 25f;
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+
+        if(Physics.Raycast(playerCam.transform.position, transform.forward, out hit, range))
+        {
+            // 레이저에 맞은 것이 있다면
+            EnemyManager enemyManager = hit.transform.GetComponent<EnemyManager>();
+
+            if(enemyManager != null)
+            {
+                enemyManager.Hit(damage);                
+            }
+        }
+    }
+}
