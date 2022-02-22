024bb8e410c94aa5cdb41b76980cce9de9976efe
Mitier26 <mitier26@gmail.com>
2022-02-22 15:45:28 +0900

    끝

--- TycoonGame01/Assets/Scripts/CamaraMovement.cs
+++ TycoonGame01/Assets/Scripts/CamaraMovement.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class CamaraMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] float speed = 1.8f;
+    [SerializeField] Transform leftBoarder;
+    [SerializeField] Transform rightBoarder;
+
+    public void Clamp(ref Vector3 value)
     {
-        
+        value.x = Mathf.Clamp(value.x, leftBoarder.position.x, rightBoarder.position.x);
+        value.y = Mathf.Clamp(value.y, leftBoarder.position.y, rightBoarder.position.y);
+        value.z = Mathf.Clamp(value.z, leftBoarder.position.z, rightBoarder.position.z);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(Input.touchCount > 0)
+        {
+            if(Input.GetTouch(0).phase == TouchPhase.Moved)
+            {
+                Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+
+                Vector3 newPos = transform.position + new Vector3(0,0, -touchDeltaPosition.x * Time.deltaTime * speed);
+            
+                Clamp(ref newPos);
+                transform.position = newPos;
+            }
+        }
     }
 }
